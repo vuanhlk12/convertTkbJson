3791e790b6a9f3aa9dad2d10556efc4a669243d8
Viet Hoang Nguyen <vhn716@uowmail.edu.au>
2021-10-02 15:13:26 +1000

    New version

--- convertTkbJson/Controller/ReadFile.cs
+++ convertTkbJson/Controller/ReadFile.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace convertTkbJson.Controller
 {
@@ -32,6 +33,19 @@ namespace convertTkbJson.Controller
             }
         }
 
+        public (string, string) getAfternoon(int number)
+        {
+            switch (number)
+            {
+                case 0: return ("13:30", "14:55");
+                //case 1: return ("08:05", "08:50");
+                case 2: return ("15:05", "16:30");
+                //case 3: return ("09:50", "10:35");
+                //case 4: return ("10:40", "11:25");
+                default: return ("", "");
+            }
+        }
+
 
         public void ReadExcelFile(string filePath)
         {
@@ -39,6 +53,7 @@ namespace convertTkbJson.Controller
             if (!file.Exists) throw new Exception("Excel file is not exist.");
             if (Path.GetExtension(filePath) != ".xlsx") throw new Exception("Config file had to .xlsx file");
             List<Classroom> classrooms = new List<Classroom>();
+            List<Classroom> afternoon = new List<Classroom>();
 
             using (ExcelPackage package = new ExcelPackage(file))
             {
@@ -51,7 +66,7 @@ namespace convertTkbJson.Controller
 
                     Classroom classroom = new Classroom();
                     classroom.Name = sheet.Cells[2, i].Value.ToString();
-                    classroom.ClassroomNumber = sheet.Cells[3, i].Value.ToString();
+                    classroom.Room = sheet.Cells[3, i].Value.ToString();
 
                     for (int k = 4; k <= 54; k += 10)
                     {
@@ -61,18 +76,49 @@ namespace convertTkbJson.Controller
                         {
                             (string start, string end) = getTime(j);
                             string Title = sheet.Cells[k + j, i].Value?.ToString() ?? "";
-                            string Teacher = sheet.Cells[k + j, i + 1].Value?.ToString() ?? "";
-                            if (Title != "") lessons.Add(new Lesson() { Title = Title, Teacher = Teacher, Start = start, End = end });
+                            string Note = sheet.Cells[k + j, i + 1].Value?.ToString() ?? "";
+                            if (Title != "") lessons.Add(new Lesson() { Title = Title, Note = $"GV: {Note}", Start = start, End = end });
                         }
 
                         classroom.Lessons.Add(lessons);
                     }
                     classrooms.Add(classroom);
                 }
+
+                for (int i = 67; i<131; i++)
+                {
+                    if (sheet.Cells[2, i].Value == null) continue;
+
+                    Classroom classroom = new Classroom();
+                    classroom.Name = sheet.Cells[2, i].Value.ToString();
+                    classroom.Room = sheet.Cells[3, i].Value?.ToString() ?? "";
+
+                    for (int k = 9; k <= 59; k += 10)
+                    {
+                        List<Lesson> lessons = new List<Lesson>();
+
+                        for (int j = 0; j < 5; j+=2)
+                        {
+                            (string start, string end) = getAfternoon(j);
+                            string Title = sheet.Cells[k + j, i].Value?.ToString() ?? "";
+                            string Note = sheet.Cells[k + j, i + 1].Value?.ToString() ?? "";
+                            if (Title != "") lessons.Add(new Lesson() { Title = Title, Note = $"GV. {Note}", Start = start, End = end });
+                        }
+
+                        classroom.Lessons.Add(lessons);
+                    }
+                    afternoon.Add(classroom);
+                }
             }
 
-            string json = JsonConvert.SerializeObject(classrooms);
-            File.WriteAllText($"{DateTimeOffset.Now.ToUnixTimeSeconds()}_converted.json", json);
+            var serializerSeetings = new JsonSerializerSettings();
+            serializerSeetings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            string json = JsonConvert.SerializeObject(classrooms, serializerSeetings);
+            File.WriteAllText($"{FolderPath}/{DateTimeOffset.Now.ToUnixTimeSeconds()}_converted.json", json);
+
+            string json2 = JsonConvert.SerializeObject(afternoon, serializerSeetings);
+            File.WriteAllText($"{FolderPath}/{DateTimeOffset.Now.ToUnixTimeSeconds()}_afternoon.json", json2);
         }
     }
 }
